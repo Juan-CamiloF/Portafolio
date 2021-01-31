6317d46e6d9858df3094e3a62bc590e7f843d8e4
Juan-CamiloF <jucamilogac@outlook.com>
2021-01-31 14:03:03 -0500

    Portafolio

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,88 +1,32 @@
-//DOM y Variables
-const inicio = document.getElementById("inicio");
-const sobreMi = document.getElementById("sobreMi");
-const proyectos = document.getElementById("proyectos");
-const contacto = document.getElementById("contacto");
 
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
-
+//Secciones
+const secciones = document.querySelectorAll(".seccion");
+//Funcion de Secciones
+function funcionSecciones(e) {
+  for (let i = 0; i < secciones.length; i++) {
+    if (e == secciones[i].id) {
+      secciones[i].className = `${e}`;
+      localStorage.setItem('ubicacion',e);
+    } else {
+      secciones[i].className = `invisible`;
+    }
+  }
+}
+//Traer del localStorage la ultima ubicacion
+function funcionLocalStorage(){
+  if(localStorage.getItem('ubicacion')){
+    funcionSecciones(localStorage.getItem('ubicacion'))
+  }
+}
+//Variables para la barra de menu
 const btnIcon = document.getElementById("icono");
 const barra = document.getElementById("barra");
 let i = 0;
-
-
-
-//Local Storage Ubicación
-const ubicacion = localStorage.getItem("lugar");
-//Si no hay nada en el localStorage
-if (!ubicacion) {
-  inicio.style.display = "";
-  sobreMi.style.display = "none";
-  proyectos.style.display = "none";
-  contacto.style.display = "none";
-  localStorage.setItem("lugar", "inicio");
-}
-//Si hay algo dentro del localStorage
-if (ubicacion) {
-  if (ubicacion == "inicio") {
-    sobreMi.style.display = "none";
-    proyectos.style.display = "none";
-    contacto.style.display = "none";
-  } else if (ubicacion == "sobreMi") {
-    inicio.style.display = "none";
-    proyectos.style.display = "none";
-    contacto.style.display = "none";
-  } else if (ubicacion == "proyectos") {
-    inicio.style.display = "none";
-    sobreMi.style.display = "none";
-    contacto.style.display = "none";
-  } else {
-    inicio.style.display = "none";
-    sobreMi.style.display = "none";
-    proyectos.style.display = "none";
-  }
-}
-
-//Eventos
-
-btn1.addEventListener("click", () => {
-  localStorage.setItem("lugar", "inicio");
-  inicio.style.display = "";
-  sobreMi.style.display = "none";
-  proyectos.style.display = "none";
-  contacto.style.display = "none";
-});
-btn2.addEventListener("click", () => {
-  localStorage.setItem("lugar", "sobreMi");
-  inicio.style.display = "none";
-  sobreMi.style.display = "";
-  proyectos.style.display = "none";
-  contacto.style.display = "none";
-});
-btn3.addEventListener("click", () => {
-  localStorage.setItem("lugar", "proyectos");
-  inicio.style.display = "none";
-  sobreMi.style.display = "none";
-  proyectos.style.display = "";
-  contacto.style.display = "none";
-});
-btn4.addEventListener("click", () => {
-  localStorage.setItem("lugar", "contacto");
-  inicio.style.display = "none";
-  sobreMi.style.display = "none";
-  proyectos.style.display = "none";
-  contacto.style.display = "";
-});
-
 //Evento de menú
-
 btnIcon.addEventListener("click", () => {
   if (i == 0) {
     barra.className = "barra dos";
-    btnIcon.innerHTML= `&#10006`;
+    btnIcon.innerHTML = `&#10006`;
     i = 1;
   } else {
     barra.classList.remove("dos");
@@ -92,6 +36,7 @@ btnIcon.addEventListener("click", () => {
   }
   cerrarBarra(i);
 });
+//Cerrar el menu si esta abierto y el tamaño aumenta
 window.addEventListener("resize", () => {
   if (screen.width > 750) {
     i = 0;
@@ -100,19 +45,22 @@ window.addEventListener("resize", () => {
   }
 });
 
-//Cerrar el menú cuando se de click
-
+//Cerrar el menú cuando se de click a un btn
+const btn = document.querySelectorAll('.btn');
+console.log(btn)
 function cerrarBarra(estado) {
   if (estado == 1) {
-    btn1.addEventListener("click", barraCerrada);
-    btn2.addEventListener("click", barraCerrada);
-    btn3.addEventListener("click", barraCerrada);
-    btn4.addEventListener("click", barraCerrada);
+    for (let i = 0; i < btn.length; i++) {
+      btn[i].addEventListener('click',barraCerrada)
+    }
   }
 }
-function barraCerrada(){
+//Funcion para quitar el menu
+function barraCerrada() {
   barra.classList.remove("dos");
   barra.className = "barra uno";
   btnIcon.innerHTML = `&#9776`;
-  i=0;
+  i = 0;
 }
+//Llamar la funcion del localStorageS
+funcionLocalStorage()
