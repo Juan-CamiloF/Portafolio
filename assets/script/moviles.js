e0ccf9a2f49d17803108e9182b343fef4ccbe90b
Juan-CamiloF <jucamilogac@outlook.com>
2021-02-04 13:00:59 -0500

    Portafolio

---
+++ assets/script/moviles.js
@@ -0,0 +1,8 @@
+//Encontrar el tamaño de la pantalla
+//Si tiene tamaño para movil entonces quitar los elementos hover
+if(screen.width <= 950){
+    document.getElementById('moviles1').classList.remove('hover');
+    document.getElementById('moviles2').classList.remove('hover');
+}
+
+
